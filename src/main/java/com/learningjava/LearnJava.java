9db229582a6930e3892608e7d1991c9146331f27
studentqa2021 <sarowerhome@gmail.com>
2022-01-14 03:39:45 -0500

    Add all my files

---
+++ src/main/java/com/learningjava/LearnJava.java
@@ -0,0 +1,11 @@
+package com.learningjava;
+
+
+public class LearnJava {
+	
+	public static void getname() {
+		
+	System.out.println("My Name Is Likhon Farazi");
+	}
+
+}
---
+++ src/test/java/com/testing/JavaTesting.java
@@ -0,0 +1,10 @@
+package com.testing;
+
+import com.learningjava.LearnJava;
+
+public class JavaTesting {
+	public static void main(String[] args) {
+		LearnJava.getname();
+	}
+
+}
